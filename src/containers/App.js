2db0371ada0dae4ff46605c3ee66fdcb6aead151
BerkatGea19 <berkat.gea@mail.ukrim.ac.id>
2022-05-27 21:43:48 +0700

    merubah h1 meiman ke berkat

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Selamat datang di website meiman</h1>
+      <h1>Selamat datang di website Berkat</h1>
     </div>
   );
 }
